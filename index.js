bb6e60e0e7d9d87f27b05b4d1c83e7ce3b3b0547
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-26 19:49:51 +0200

    Add UI and functionality to rps game

--- index.js
+++ index.js
@@ -7,37 +7,45 @@ function getComputerChoice (computerChoiceInt, computerChoice = "")  {
   return computerChoice;
 }
 
-function getHumanChoice (humanChoice) {
-  humanChoice = prompt("Please enter a choice: Rock, Paper or Scissors");
-  humanChoice = humanChoice.toLowerCase();
-  while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
-    humanChoice = prompt("Please enter a valid choice");
-    humanChoice = humanChoice.toLowerCase();
-  }
-  return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice) {
+  switch (computerChoice) {
+    case "rock":
+      cImage.src = "rock.svg";
+      break;
+    case "paper":
+      cImage.src = "paper.svg";
+      break;
+    case "scissors":
+      cImage.src = "scissors.svg";
+      break;
+  }
+  
   if (humanChoice === computerChoice) {
-    console.log(humanChoice, computerChoice);
     console.log("It's a tie");
+    announce.textContent = "It's a tie";
   } else if (humanChoice == "rock" && computerChoice == "paper") {
     console.log("You loose");
+    announce.textContent = "You loose";
     computerScore++;
   } else if (humanChoice == "rock" && computerChoice == "scissors") {
     console.log("You win");
+    announce.textContent = "You win";
     humanScore++;
   } else if (humanChoice == "paper" && computerChoice == "scissors") {
     console.log("You loose");
+    announce.textContent = "You loose";
     computerScore++;
   }else if (humanChoice == "paper" && computerChoice == "rock") {
     console.log("You win");
+    announce.textContent = "You win";
     humanScore++;
   } else if (humanChoice == "scissors" && computerChoice == "rock") {
     console.log("You loose");
+    announce.textContent = "You loose";
     computerScore++;
   } else if (humanChoice == "scissors" && computerChoice == "paper") {
     console.log("You win");
+    announce.textContent = "You win";
     humanScore++;
   }
 }
@@ -45,20 +53,59 @@ function playRound(humanChoice, computerChoice) {
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    
-    playRound(humanChoice, computerChoice);
-    console.log(`Human chose ${humanChoice}, Computer chose ${computerChoice}`)
-    console.log(`Human score ${humanScore}`)
-    console.log(`Computer score ${computerScore}`)
-  }
-  humanScore > computerScore ? console.log("You won the game!") :
-  humanScore < computerScore ? console.log("You lost the game!") :
-  console.log("You tied!");
-} 
+//UI changes are made below this line
+
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const buttonReset = document.querySelector(".reset");
+const pImage = document.querySelector("#playerImage");
+const cImage = document.querySelector("#computerImage");
+
+pImage.src = "scissors.svg"
+
+let playerScoreTable = document.querySelector(".playerScore");
+let computerScoreTable = document.querySelector(".computerScore");
+let announce = document.querySelector(".announcment");
+
+playerScoreTable.textContent = humanScore;
+computerScoreTable.textContent = computerScore;
+
+buttonRock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+  playerScoreTable.textContent = humanScore;
+  computerScoreTable.textContent = computerScore;
+  pImage.src = "rock.svg"
+
+})
+
+buttonPaper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+  playerScoreTable.textContent = humanScore;
+  computerScoreTable.textContent = computerScore;
+  pImage.src = "paper.svg"
+})
+
+buttonScissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+  playerScoreTable.textContent = humanScore;
+  computerScoreTable.textContent = computerScore;
+  pImage.src = "scissors.svg"
+})
+
+buttonReset.addEventListener("click", () => {
+  humanScore = 0;
+  computerScore = 0;
+  playerScoreTable.textContent = humanScore;
+  computerScoreTable.textContent = computerScore;
+  console.log(playerScoreTable, computerScoreTable);
+  console.log(humanScore, computerScore);
+  announce.textContent = "";
+})
 
 
-playGame();
+
+
+
+
+
